9b147a747abb024690006d8554043f35432c364c
LeandroCeleodonio <leandroceledonio04@gmail.com>
2019-11-16 15:34:52 -0400

    Crear Vehiculos, Maqueta sesion subir imagenes

---
+++ MyRentCar/Rent.Web/Controllers/VehiculosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rent.Web.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearVehiculo()
+        {
+            return View();
+        }
+    }
+}
